e32bb884568eda1d216da40ea923c32dea46fc56
Pedro Vitorino <pedrolucasofici@gmail.com>
2025-03-23 16:12:39 -0300

    vetores

---
+++ src/vetores/aula/Aula2.java
@@ -0,0 +1,32 @@
+package vetores.aula;
+
+import vetores.entities.product;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula2 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        product[] vect = new product[n];
+
+        for (int i = 0; i < vect.length; i++) {
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new product(name, price);
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < vect.length; i++) {
+            sum += vect[i].getPrice();
+        }
+        double avg = sum / vect.length;
+
+        System.out.printf("AVERAGE PRICE = %.2f%n", avg);
+
+        sc.close();
+    }
+}
---
+++ src/vetores/exercicio/exercicio1.java
@@ -0,0 +1,29 @@
+package vetores.exercicio;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio1 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos numeros voce vai digitar? ");
+        int n = sc.nextInt();
+
+        int[] vect = new int[n];
+
+        for (int i = 0; i < vect.length; i++) {
+            System.out.printf("Digite um numero: ");
+            vect[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i] < 0) {
+                System.out.printf("%d\n", vect[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
