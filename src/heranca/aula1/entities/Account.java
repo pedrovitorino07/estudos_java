b1feea86a89338b650f49d27a4b43f3797cc57aa
Pedro Vitorino <pedrolucasofici@gmail.com>
2025-05-15 12:59:47 -0300

    herança/polimorfismo

---
+++ src/heranca/aula1/entities/Account.java
@@ -0,0 +1,45 @@
+package heranca.aula1.entities;
+
+public class Account {
+
+    private Integer number;
+    private String holder;
+    protected Double balance;
+
+    public Account (){
+    }
+
+    public Account(Integer number, String holder, Double balance) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public void withdraw(Double amount) {
+        this.balance -= amount;
+    }
+
+    public void deposit(Double amount) {
+        this.balance += amount;
+    }
+}
