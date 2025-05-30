c2d560681f274c53c7656ba30f1ed3321a266e95
Pedro Vitorino <pedrolucasofici@gmail.com>
2025-05-30 15:07:23 -0300

    Map

--- src/generics/aula7/Program.java
+++ src/generics/aula7/Program.java
@@ -10,7 +10,7 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Set<Product> set = new TreeSet<>();
+        Set<Product> set = new HashSet<>();
 
         set.add(new Product("TV", 900.0));
         set.add(new Product("Notebook", 1200.0));
