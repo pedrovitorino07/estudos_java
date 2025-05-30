901ef8f4a7702ed535dd6fd1c295a39b2c9a6b4c
Pedro Vitorino <pedrolucasofici@gmail.com>
2025-05-30 07:46:43 -0300

    set/2

---
+++ src/generics/aula7/Program.java
@@ -0,0 +1,25 @@
+package generics.aula7;
+
+import generics.aula7.entities.Product;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Set<Product> set = new TreeSet<>();
+
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+
+        Product prod = new Product("Notebook", 1200.0);
+
+        for (Product p : set) {
+            System.out.println(p);
+        }
+    }
+}
