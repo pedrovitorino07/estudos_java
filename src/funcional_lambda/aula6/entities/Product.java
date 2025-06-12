46d223a0a55578c1f69f2e60331363c008250de1
Pedro Vitorino <pedrolucasofici@gmail.com>
2025-06-12 10:52:33 -0300

    Pipeline

---
+++ src/funcional_lambda/aula6/entities/Product.java
@@ -0,0 +1,33 @@
+package funcional_lambda.aula6.entities;
+
+public class Product {
+
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", " + String.format("%.2f", price);
+    }
+}
